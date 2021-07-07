8a05bf1e999a0120578872d629a22388ee0762c1
neilburt <neil.burt@comcast.net>
2021-07-07 17:56:25 -0500

    Created folder structure, built class js files, started index.js with requires, created test files, worked on HTML template.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const fs = require('fs')
+const inquirer = require('Inquirer')
+const jest = require('Jest')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+
+
 // require in a package that has all of the functions you need
 // that package can require in the classes
-
-const inquirer  = require("inquirer")
 
 askForManagerInfo();{
   inquirer
