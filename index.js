1c4b1ea388c9d819970233cdc1d8639b335318a3
neilburt <neil.burt@comcast.net>
2021-07-07 15:17:25 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+// require in a package that has all of the functions you need
+// that package can require in the classes
+
+const inquirer  = require("inquirer")
+
+askForManagerInfo();{
+  inquirer
+    .prompt([
+
+  ]).then(makeManager)
+}
